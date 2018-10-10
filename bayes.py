cc773d3ab80ec5877a20059e3c8b24a17ccfc160
Sharatgm <A01205254@itesm.mx>
2018-10-09 20:26:01 -0500

    Create CPT and save parents

--- bayes.py
+++ bayes.py
@@ -1,37 +1,25 @@
 import fileinput
 import time
-import pprint
+import pprint as pp
 
 def format_nodes(line):
-    # First the \n is removed and then the line is splited in the ","
+    # Create the nodes given their names
     names = line.replace("\n","").split(",")
 
-    # This will create an array of nodes
-    nodes = []
+    nodes = {}
     for i in range (0,len(names)):
-        # Each node will be in the format {'name' = 'rain', 'probabilities' = [Array of the probabilities], 'parents' = [array of the parents] }
-        nodes.append({'name': names[i], 'probabilities': [], 'parents': []})
+        nodes[names[i]] = {'probabilities': [], 'parents': []}
     return nodes
 
 def add_probability(nodes, line):
-    # First it splits the line in the "=" [Description, value]
-    prob = line.split("=")
-
-    # Split the description in the |.
-    #    Cases:
-    #       If there is a condition ( | ):  [[Node, Condition], Value]
-    #       If there is not a condition: [Description, Value]
-    prob[0] = prob[0].split("|")
-    if (len(prob[0]) > 1):
-        # Si hubo condición
-
-    else:
-        # No hubo condición y es el nodo raíz
-
+    # Add each probability to the CPT of each node
+    prob = line.replace('|',' ').replace(',',' ').replace('=',' ').split()
+    name = prob[0][1:]
+    nodes[name]['probabilities'].append(prob)
+    return nodes
 
 
 def main():
-    # Read file
     file_input = fileinput.input()
 
     # Nodes are formated in the function format_nodes()
@@ -42,7 +30,15 @@ def main():
     probabilities = []
     for i in range (0,num_prob):
         line = file_input.readline().replace("\n", "")
-        probabilities.append(line.split("="))
+        add_probability(nodes, line)
+    # Saving parents
+    for node in nodes:
+        parents = nodes[node]['probabilities'][0][1:len(nodes[node]['probabilities'][0])-1]
+        for i in range(0,len(parents)):
+            parents[i] = parents[i][1:]
+        nodes[node]['parents'] = parents
+
+    pp.pprint(nodes)
 
     # Queries
     num_que = int(file_input.readline())
@@ -50,10 +46,6 @@ def main():
     for i in range(0, num_que):
         line = file_input.readline().replace("\n", "")
         queries.append(line.split("="))
-
-    print("NODES = ", nodes)
-    print("PROBABILITIES = ", probabilities)
-    print("QUERIES = ", queries)
 
 
 if __name__ == "__main__":
